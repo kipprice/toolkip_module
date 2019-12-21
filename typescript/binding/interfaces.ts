1b5de4a0d5c1c93ebf74e24e8153e05e4da41d51
Kip Price <kip@codecademy.com>
2019-12-21 18:55:53 -0500

    convert model binding helpers to modules

---
+++ typescript/binding/interfaces.ts
@@ -0,0 +1,22 @@
+import { IEqualityFunction } from '../interfaces/comparable';
+
+export interface BoundEvalFunction<T> {
+    (): T
+}
+
+export interface BoundUpdateFunction<T> {
+    (newVal: T): void;
+}
+
+export interface BoundDeleteFunction {
+    (): boolean;
+}
+
+export interface IBindingDetails<T> {
+    id: string;
+    eval: BoundEvalFunction<T>;
+    update: BoundUpdateFunction<T>;
+    delete: BoundDeleteFunction;
+    lastValue: T;
+    equals: IEqualityFunction<T>;
+}
