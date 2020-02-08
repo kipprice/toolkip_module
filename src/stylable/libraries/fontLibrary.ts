98e69e9804021d4ec7ca5dbe94f25b849f8138fa
Kip Price <kip@codecademy.com>
2020-02-07 22:47:31 -0500

    remove commented out code

--- src/stylable/libraries/fontLibrary.ts
+++ src/stylable/libraries/fontLibrary.ts
@@ -12,7 +12,6 @@ import { IFontFaceDefinition } from "../../styleHelpers";
  */
 class _FontLibrary extends _Library {
     
-
     //.....................
     //#region PROPERTIES
     
@@ -21,46 +20,6 @@ class _FontLibrary extends _Library {
     //#endregion
     //.....................
 
-    
-
-    // TODO: add any custom logic we need for fonts
-
-    // /**
-    //  * _mergeIntoFonts
-    //  * ----------------------------------------------------------------------------
-    //  * @param fonts 
-    //  * 
-    //  */
-    // protected _mergeFonts(fonts: IFontFaceDefinition[]): IFontFaceDefinition {
-    //     let out: IDictionary<IFontFaceDefinition> = {};
-        
-    //     for (let f of fonts) {
-    //         map(f, (fontDef: IFontFaceDefinition[], fontName: string) => {
-    //             out[fontName] = fontDef;
-    //         });
-    //     }
-        
-    // }
-
-    //     /**
-    //  * _createFontStyles
-    //  * ----------------------------------------------------------------------------
-    //  * @param forceOverride 
-    //  */
-    // protected static _createFontStyles(forceOverride?: boolean): void {
-    //     // add the font-family pieces
-    //     let fonts: ICustomFonts = Stylable._customPageFonts;
-    //     if (!this._customFontElem) { 
-    //         this._customFontElem = createStyleElement(false); 
-    //         document.head.appendChild(this._customFontElem);
-    //     }
-    //     this._customFontElem.innerHTML = "";
-
-    //     map(fonts, (fontDef: IFontFaceDefinition[], fontName: string) => {
-    //         let tmpElem: HTMLStyleElement = createFontDefinition(fontName, fontDef, true);
-    //         this._customFontElem.innerHTML += tmpElem.innerHTML;
-    //     });
-    // }
 }
 
 export const FontLibrary = new _FontLibrary();
