9fcdc27fcdedad745814c910498acb1f49eae607
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-05-25 21:01:30 -0400

    export everything we need to

--- packages/model/src/index.ts
+++ packages/model/src/index.ts
@@ -1,9 +1,11 @@
 export * from './_shared';
+export * from './_typeguards';
 export * from './abstractClasses';
 export * from './primitiveModels';
 export * from './objectModels';
 export * from './arrayModels';
 export * from './helpers';
+export * from './transforms';
 
 import { setupModelWrapping } from './helpers/modelFactory';
 setupModelWrapping();
